6cd489acc0798e2b0ddb6971f513e8b6854c053d
Aman <aman.pateldtc@gmail.com>
2023-10-16 19:42:44 +0530

    Deploy

--- database/db.js
+++ database/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 
 const connectToMongo = ()=>{
-    mongoose.connect("mongodb+srv://Amanpatel:kZPItoneNPVCvaIk@cluster0.5ik8wmj.mongodb.net/?retryWrites=true&w=majority");
+    mongoose.connect(process.env.MONGODB_URI);
     mongoose.connection.on('connected', () => console.log('Connected'));
     mongoose.connection.on('error', (err) => console.log('Connection failed with - ',err));
 }
